3ada195a7dd0410efb3f4dc380fe8bb57a07dbec
QuangTruong2612 <quangtruong0905774841@gmail.com>
2026-01-23 20:16:14 +0700

    update

--- src/Project_1/components/training_model.py
+++ src/Project_1/components/training_model.py
@@ -42,6 +42,8 @@ class TrainingModel:
             dagshub.init(repo_owner=self.config.repo_owner, repo_name=self.config.repo_name, mlflow=True)
         else:
             print("Running in CI/CD, using existing environment variables.")
+        print(os.environ)
+        print(self.device)
         exp_name = "Project_1_Tracking_Model"
         print(f"Setting MLflow experiment to: {exp_name}")
         mlflow.set_experiment(exp_name)
