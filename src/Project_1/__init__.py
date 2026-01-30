adf1c72ffcb2cb0662957e566da1b871f375bdbc
QuangTruong2612 <quangtruong0905774841@gmail.com>
2026-01-30 15:39:33 +0700

    update

--- src/Project_1/__init__.py
+++ src/Project_1/__init__.py
@@ -15,4 +15,4 @@ logging.basicConfig(
     handlers=[logging.FileHandler(log_filepath), logging.StreamHandler(sys.stdout)],
 )
 
-logger = logging.getLogger("cnnClassifierLogger")
+logger = logging.getLogger("Project_1_logger")
